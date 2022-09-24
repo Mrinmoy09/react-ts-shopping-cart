87db4a6c322cbef904491bd5cde104ee8c75faee
Mrinmoy09 <mrinmoy0908@gmail.com>
2022-09-25 01:33:50 +0600

    store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import storeItems from '../data/items.json'
+import {Row,Col} from 'react-bootstrap'
+import StoreItem from '../components/StoreItem';
 
 const Store = () => {
+    
     return (
-        <div>
-            <h1>Store</h1>
-        </div>
+        <>
+          <Row md={2} lg={3} className="g-3">
+            {storeItems.map(item=>
+                <Col sm={1} key={item.id}><StoreItem {...item}></StoreItem></Col>)
+            }
+          </Row>  
+        </>
     );
 };
 
